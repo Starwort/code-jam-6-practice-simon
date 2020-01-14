78ebe2f8cea51d8ce4c08e2b47fb3defcab73015
Muhammad Khan <MuhammadKhan1607@gmail.com>
2020-01-14 18:01:39 +0500

    Create the curved_interface branch

--- gui/root.py
+++ gui/root.py
@@ -1,6 +1,9 @@
 """Python file to contain declaration of root screens"""
 
 from kivy.uix.screenmanager import Screen
+from kivy.uix.widget import Widget
+from kivy.properties import ListProperty, NumericProperty
+from kivy.animation import Animation
 
 
 class StartScreen(Screen):
@@ -13,3 +16,28 @@ class GameScreen(Screen):
     """Class for the game screen"""
 
     pass
+
+
+class SimonButton(Widget):
+    """A custom curved shape button for the game"""
+
+    background_color = ListProperty()
+    start_angle = NumericProperty(0)
+    end_angle = NumericProperty(360)
+    radius = NumericProperty()
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            anim = Animation(
+                background_color=[*self.background_color[:-1], 0.4], d=0.2
+            ) + Animation(background_color=self.background_color, d=0.2)
+            anim.start(self)
+
+    def on_size(self, _, value):
+        self.radius = min(self.parent.size) / 2.5
+
+    #   Essentially a property but just a setter
+    def __setattr__(self, name, value):
+        if name == "angle":
+            self.start_angle, self.end_angle = value
+        super().__setattr__(name, value)
